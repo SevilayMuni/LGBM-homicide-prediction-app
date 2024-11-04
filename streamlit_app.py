172c668ce47d36ecd96a0567ce8d8a611d51b416
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-11-04 16:32:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pickle
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
 
-st.title('🕵🏻 Homicide Solve Prediction App 👩🏻‍💻')
+st.title('🕵🏻 Homicide Solve Prediction App')
 st.write('The project is dedicated to all murder victims and their families whose justice has not been served yet.')
 st.write('The project aims to conduct data science research and demonstrate the importance of accurately accounting for unsolved homicides within communities. ')
 st.write('The model data source is Murder Accountability Project')
