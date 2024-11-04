2b29cd76a9e1f35dbb6c03f18d68047ea2c8da10
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-11-04 16:31:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('🕵🏻 Homicide Solve Prediction App 👩🏻‍💻')
+st.write('The project is dedicated to all murder victims and their families whose justice has not been served yet.')
+st.write('The project aims to conduct data science research and demonstrate the importance of accurately accounting for unsolved homicides within communities. ')
+st.write('The model data source is Murder Accountability Project')
