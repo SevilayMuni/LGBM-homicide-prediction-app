10f447c6ced505fb9ed5b2ab56ae75e93fdc9f51
Sevilay Munire Girgin, MSc. <157280744+SevilayMuni@users.noreply.github.com>
2024-11-09 13:17:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ pickled_model_app = joblib.load('./model_app.pkl')
 
 st.title('🕵🏻 Homicide Solve Prediction App')
 st.write('The project is dedicated to all murder victims and their families whose justice has not been served yet.')
-st.info('The project aims to conduct data science research and demonstrate the importance of accurately accounting for unsolved homicides within communities. The model data source is Murder Accountability Project')
+st.success('The project aims to conduct data science research and demonstrate the importance of accurately accounting for unsolved homicides within communities.')
+st.write('The model data source is Murder Accountability Project.')
 
 # Input features
 with st.sidebar:
